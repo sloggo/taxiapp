20460cd77a05972f511026014c7f80cce2938e92
sloggo <70816908+sloggo@users.noreply.github.com>
2023-11-27 23:23:57 +0000

    Add getters for the Location class and add Map class

--- src/main/java/org/taxiapp/classes/Location.java
+++ src/main/java/org/taxiapp/classes/Location.java
@@ -8,6 +8,22 @@ public class Location {
     private List<Taxi> taxis;
     private List<User> users;
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public List<Taxi> getTaxis() {
+        return taxis;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
     public Location(int x, int y){
         this.x = x;
         this.y = y;
