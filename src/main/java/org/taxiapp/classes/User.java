57e90211b98afb439864db9ff1dbb5004263d552
sloggo <70816908+sloggo@users.noreply.github.com>
2023-11-28 16:18:15 +0000

    Implement attributes of user class and implement getters

--- src/main/java/org/taxiapp/classes/User.java
+++ src/main/java/org/taxiapp/classes/User.java
@@ -1,5 +1,19 @@
 package org.taxiapp.classes;
 
-public class User {
+public abstract class User {
+    private Location location;
+    private double rating;
+    private Ride currentRide;
 
+    public Location getLocation() {
+        return location;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public Ride getCurrentRide() {
+        return currentRide;
+    }
 }
