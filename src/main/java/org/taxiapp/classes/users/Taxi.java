60fb61a448d1edfadf941bc72bfd29c84965310d
sloggo <70816908+sloggo@users.noreply.github.com>
2023-11-30 15:49:48 +0000

    Update user locations on map using classes and polymorphism, also add attribute type to each user

--- src/main/java/org/taxiapp/classes/users/Taxi.java
+++ src/main/java/org/taxiapp/classes/users/Taxi.java
@@ -1,14 +1,14 @@
 package org.taxiapp.classes.users;
 
 import org.taxiapp.classes.Location;
+import org.taxiapp.classes.Map;
 import org.taxiapp.classes.users.User;
 
 public class Taxi extends User {
-    private String registration;
     private int rate; // eur/km base rate
     public Taxi(Location location, String registration){
-        super(location);
-        this.registration = registration;
+        super(location, registration);
         this.rate = 10;
+        this.type = "taxi";
     }
 }
--- src/main/java/org/taxiapp/classes/users/User.java
+++ src/main/java/org/taxiapp/classes/users/User.java
@@ -1,19 +1,29 @@
 package org.taxiapp.classes.users;
 
 import org.taxiapp.classes.Location;
+import org.taxiapp.classes.Map;
 import org.taxiapp.classes.Rating;
 import org.taxiapp.classes.Ride;
 
 public abstract class User {
-    public User(Location location){
-        this.location = location;
+    public User(Location location, String id){
+        setLocation(location);
+        this.id = id;
+        this.type = "invalid";
     }
+    protected Map map;
     protected Location location;
     protected Rating[] ratings;
     protected Ride currentRide;
+    protected String id;
+    protected String type;
 
     public Location getLocation() {
         return location;
+    }
+    public void setLocation(Location locationIn) {
+        location = locationIn;
+        locationIn.addUser(this);
     }
 
     public double getRating() {
@@ -23,4 +33,7 @@ public abstract class User {
     public Ride getCurrentRide() {
         return currentRide;
     }
+    public String getId() { return id; }
+
+    public String getType() {return type;}
 }
