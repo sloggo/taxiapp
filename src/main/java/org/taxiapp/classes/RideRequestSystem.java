b550a45eff34c9c06cd723be50b8ba0d59c15803
Ruairí Glackin <ruairiglackin@gmail.com>
2023-12-05 15:09:15 +0000

    rideSystem updated

--- src/main/java/org/taxiapp/classes/RideRequestSystem.java
+++ src/main/java/org/taxiapp/classes/RideRequestSystem.java
@@ -8,17 +8,14 @@ import java.util.List;
 
 public class RideRequestSystem  {
     private List<Observer> observers = new ArrayList();
-
     public void attachObserver(Observer observer) {
         observers.add(observer);
     }
 
-
     public void detachObserver(Observer observer) {
         observers.remove(observer);
     }
-
-
+    
     public void notifyObservers() {
         for (Observer o :observers) {
             o.update();
