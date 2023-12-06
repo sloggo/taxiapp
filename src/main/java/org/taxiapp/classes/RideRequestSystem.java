3c7854ec1a6d4c29e175ef5e5ef87adcc249e367
sloggo <70816908+sloggo@users.noreply.github.com>
2023-12-06 11:09:55 +0000

    Create Subject interface for the observer design pattern

--- src/main/java/org/taxiapp/classes/RideRequestSystem.java
+++ src/main/java/org/taxiapp/classes/RideRequestSystem.java
@@ -6,16 +6,17 @@ import org.taxiapp.interfaces.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RideRequestSystem  {
+public class RideRequestSystem implements Subject {
     private List<Observer> observers = new ArrayList();
+    @Override
     public void attachObserver(Observer observer) {
         observers.add(observer);
     }
-
+    @Override
     public void detachObserver(Observer observer) {
         observers.remove(observer);
     }
-    
+    @Override
     public void notifyObservers() {
         for (Observer o :observers) {
             o.update();
