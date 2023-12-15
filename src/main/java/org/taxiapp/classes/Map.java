e7a6eaaab42d6bd62db1c8493dbb48aad126e21e
sloggo <70816908+sloggo@users.noreply.github.com>
2023-12-15 12:17:57 +0000

    Add taxi writing and reading to csv

--- src/main/java/org/taxiapp/classes/Map.java
+++ src/main/java/org/taxiapp/classes/Map.java
@@ -3,17 +3,23 @@ package org.taxiapp.classes;
 import org.taxiapp.classes.users.Customer;
 import org.taxiapp.classes.users.Taxi;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
 
 public class Map {
+    private boolean test;
+    private String id;
     private Location[][] grid;
     private int mapRadius;
     private LinkedList<Taxi> taxis;
     private LinkedList<Customer> customers;
     public Map(int r){
+        this.test = false;
+        this.id = UUID.randomUUID().toString();
         this.customers = new LinkedList<>();
         this.taxis = new LinkedList<>();
         this.grid = new Location[r][r];
@@ -26,6 +32,28 @@ public class Map {
         }
     }
 
+    public Map(int r, String id, boolean test) throws IOException {
+        this.test = test;
+        this.id = id;
+        this.customers = new LinkedList<>();
+        this.taxis = new LinkedList<>();
+        this.grid = new Location[r][r];
+        this.mapRadius = r;
+
+        for(int i=0; i<r; i++) { // columns
+            for (int j = 0; j < r; j++) { // rows
+                this.grid[i][j] = new Location(i, j); // init new location item;
+            }
+        }
+
+        if(!test){
+            importTaxis();
+        }
+    }
+    public String getId(){
+        return id;
+    }
+
     public void printMap(){
         for(int i = 0; i< getMapRadius(); i++) { // columns
             for (int j = 0; j < getMapRadius(); j++) { // rows
@@ -35,6 +63,28 @@ public class Map {
             System.out.println("");
         }
     }
+
+    public void importTaxis() throws IOException{
+        File yourFile = new File(id+"-taxis.csv");
+        yourFile.createNewFile(); // if file already exists will do nothing
+        try (BufferedReader reader = new BufferedReader(new FileReader(id+"-taxis.csv"))) {
+            String newLine;
+            while ((newLine = reader.readLine()) != null) {
+                // Check if the line is not empty or just whitespace
+                if (!newLine.trim().isEmpty()) {
+                    String[] values = newLine.split(",");
+                    if (values[0].equals(id)) {
+                        int[] ratings = new int[values.length - 5];
+                        for (int i = 5; i < values.length; i++) {
+                            ratings[i - 5] = Integer.parseInt(values[i]);
+                        }
+                        new Taxi(values[1], this, Integer.parseInt(values[2]), Integer.parseInt(values[3]), Integer.parseInt(values[4]), ratings);
+                    }
+                }
+            }
+        }
+    }
+
 
     public Location getRandomLoc(){
         Random rand = new Random();
